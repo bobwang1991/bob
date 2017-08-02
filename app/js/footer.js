4d0862472990432d47db73062002a9ee503eba64
bobwang1991 <bob_1991@qq.com>
2017-08-02 19:19:49 +0800

    修改了footer模板提取出来！正式上线！

---
+++ app/js/footer.js
@@ -0,0 +1,12 @@
+
+function footer() {
+    var footer =
+        '<div class="center">'+
+        '<div class="qrChat"><img src="http://bob1991-1253674698.cossh.myqcloud.com/personalWeb/qrChat.jpg" alt=""></div>'+
+        '<div class="server">服务热线：180-1718-7988（个人）服务时间9:00-19:00</div>'+
+        '<div class="server">'+
+        'Copyright © 2015-2017 Bob 上海Bob出版社'+
+        '</div>'+
+        '</div>';
+    $('#footer').html(footer);
+}
